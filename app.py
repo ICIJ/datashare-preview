5d7f948bebfaa5a9839830a96fd72d21774cedc1
Pierre Romera <hello@pirhoo.com>
2019-06-25 16:09:50 +0200

    Refactored to download file only once

--- app.py
+++ app.py
@@ -1,63 +1,16 @@
 import os
-import requests
 
-from pathlib import Path
-from functools import lru_cache
-from tempfile import mktemp
 from flask import Flask, send_file, abort, request, jsonify
 from flask_cors import CORS
-from preview_generator.manager import PreviewManager
-from pathlib import Path
-from requests.compat import urljoin
-from utils import hash_dict
+from preview import get_jpeg_preview, get_size_width, is_content_type_previewable, build_preview_manager
+from document import delete_expired_documents, download_document, build_document_meta_url, check_user_authorization
+from document import DocumentUnauthorized, DocumentNotPreviewable, DocumentTooBig
 
-CACHE_PATH = os.environ.get('CACHE_PATH', './cache')
-DS_HOST = os.environ.get('DS_HOST', 'http://localhost:8080')
-DS_DOCUMENT_PATH = os.environ.get('DS_DOCUMENT_PATH', '/api/index/src/%s/%s')
 DS_SESSION_COOKIE_NAME = '_ds_session_id'
 DS_SESSION_HEADER_NAME = 'X-Ds-Session-Id'
 
-sizes = dict(xs=80, sm=310, md=540, lg=720, xl=960)
-
 app = Flask(__name__)
 CORS(app)
-
-def get_jpeg_preview(params, cache_path = CACHE_PATH):
-    manager = PreviewManager(cache_path, create_folder = True)
-    return manager.get_jpeg_preview(**params)
-
-def get_size_width(size):
-    if str(size).isnumeric():
-        return int(size)
-    else:
-        return sizes.get(size, sizes.get('xs'))
-
-def get_document_url(index, id, routing = None):
-    url = urljoin(DS_HOST, DS_DOCUMENT_PATH % (index, id))
-    # Optional routing parameter
-    if routing is not None: url = urljoin(url, '?routing=%s' % routing)
-    return url
-
-def download_document_with_steam(url, cookies):
-    target_path = mktemp()
-    response = requests.get(url, stream=True, cookies=cookies)
-    handle = open(target_path, "wb")
-    for chunk in response.iter_content(chunk_size=1024):
-        if chunk: handle.write(chunk)
-    return target_path
-
-@hash_dict
-@lru_cache()
-def download_document_once(url, cookies):
-    return download_document_with_steam(url, cookies)
-
-def download_document(url, cookies):
-    target_path = download_document_once(url, cookies)
-    # Ensure the file style exist
-    if not Path(target_path).exists():
-        download_document_once.cache_clear()
-        target_path = download_document_once(url, cookies)
-    return target_path
 
 def has_session_cookie():
     return DS_SESSION_COOKIE_NAME in request.cookies
@@ -65,7 +18,7 @@ def has_session_cookie():
 def has_session_header():
     return DS_SESSION_HEADER_NAME in request.headers
 
-def get_cookies_from_forwarded_headers():
+def get_cookies_from_forwarded_headers(request):
     cookies = request.cookies.copy()
     # DS_SESSION_COOKIE_NAME is present? Just return the request cookies
     if not has_session_cookie() and has_session_header():
@@ -78,35 +31,35 @@ def get_preview_generator_params(index, id):
     routing = request.args.get('routing', None)
     size = request.args.get('size', 'xs')
     page = request.args.get('page', 0)
-    # Build the document URL
-    document_url = get_document_url(index, id, routing)
+    cookies = get_cookies_from_forwarded_headers(request)
+    # Some files might had expired
+    delete_expired_documents()
+    # Beck the user can access the doucment
+    check_user_authorization(index, id, routing, cookies)
     # Download the document and return the temporary filepath
-    file_path = download_document(document_url, get_cookies_from_forwarded_headers())
+    file_path = download_document(index, id, routing, cookies)
     return dict(file_path=file_path, height=get_size_width(size), page=int(page))
 
 @app.route('/api/v1/thumbnail/<string:index>/<string:id>', methods=['GET'])
 @app.route('/api/v1/thumbnail/<string:index>/<string:id>.jpg', methods=['GET'])
 def thumbnail(index, id):
-    params = get_preview_generator_params(index, id)
-    # The file is not authorized
-    if params['file_path'] is None: abort(401)
-    # If it not working, PreviewManager will raise an exception
-    try: return send_file(get_jpeg_preview(params), as_attachment=False)
-    # Silently fail
-    except Exception as e: abort(500, e)
+    try:
+        params = get_preview_generator_params(index, id)
+        return send_file(get_jpeg_preview(params), as_attachment=False)
+    except DocumentTooBig: abort(509)
+    except DocumentNotPreviewable: abort(403)
+    except DocumentUnauthorized: abort(401)
+    except Exception: abort(500)
 
 @app.route('/api/v1/thumbnail/<string:index>/<string:id>.json', methods=['GET'])
 def info(index, id):
-    params = get_preview_generator_params(index, id)
-    # The file is not authorized
-    if params['file_path'] is None: abort(401)
-    # If it not working, PreviewManager will raise an exception
+    manager = build_preview_manager()
     try:
-        manager = PreviewManager(CACHE_PATH, create_folder = True)
-        return jsonify({
-            'pages': manager.get_page_nb(params['file_path']),
-            'minetype': manager.get_mimetype(params['file_path']),
-            'previewable': manager.has_jpeg_preview(params['file_path'])
-        })
-    # Silently fail
-    except Exception as e: abort(500, e)
+        params = get_preview_generator_params(index, id)
+        pages = manager.get_page_nb(params['file_path'])
+        return jsonify({ 'pages': pages, 'previewable': True })
+    except DocumentNotPreviewable:
+        return jsonify({ 'pages': 0, 'previewable': False })
+    except DocumentTooBig: abort(509)
+    except DocumentUnauthorized: abort(401)
+    except Exception: abort(500)
---
+++ document.py
@@ -0,0 +1,81 @@
+import os
+import requests
+
+from requests.compat import urljoin
+from datetime import datetime
+from tempfile import gettempdir
+from glob import glob
+from pathlib import Path
+from preview import is_content_type_previewable
+
+CACHE_PATH = os.environ.get('CACHE_PATH', gettempdir())
+DS_HOST = os.environ.get('DS_HOST', 'http://localhost:8080')
+DS_FILE_PREFIX = os.environ.get('DS_FILE_PREFIX', 'ds-preview-')
+DS_DOCUMENT_META_PATH = os.environ.get('DS_DOCUMENT_META_PATH', '/api/index/search/%s/doc/%s')
+DS_DOCUMENT_SRC_PATH = os.environ.get('DS_DOCUMENT_SRC_PATH', '/api/index/src/%s/%s')
+DS_DOCUMENT_MAX_SIZE = 50e6 # 50 Mo
+DS_DOCUMENT_MAX_AGE = 60 * 60 * 3 # 3 hours
+
+class DocumentUnauthorized(Exception): pass
+class DocumentNotPreviewable(Exception): pass
+class DocumentTooBig(Exception): pass
+
+def build_document_meta_url(index, id, routing = None):
+    url = urljoin(DS_HOST, DS_DOCUMENT_META_PATH % (index, id))
+    # Optional routing parameter
+    if routing is not None: url = urljoin(url, '?_source=contentLength,contentType&routing=%s' % routing)
+    return url
+
+def build_document_src_url(index, id, routing = None):
+    url = urljoin(DS_HOST, DS_DOCUMENT_SRC_PATH % (index, id))
+    # Optional routing parameter
+    if routing is not None: url = urljoin(url, '?routing=%s' % routing)
+    return url
+
+def build_document_target_path(index, id, routing = None):
+    file_name = '%s-%s-%s' % (DS_FILE_PREFIX, index, id)
+    return os.path.join(CACHE_PATH, file_name)
+
+def get_file_age(file):
+    return datetime.now().timestamp() - os.path.getmtime(file)
+
+def is_file_expired(file):
+    return get_file_age(file) > DS_DOCUMENT_MAX_AGE
+
+def expired_documents():
+    files = glob(os.path.join(CACHE_PATH, '%s*' % DS_FILE_PREFIX))
+    return [ file for file in files if is_file_expired(file) ]
+
+def delete_expired_documents():
+    for file_path in expired_documents():
+        os.remove(file_path)
+
+def download_document_with_steam(url, target_path, cookies):
+    response = requests.get(url, stream=True, cookies=cookies)
+    handle = open(target_path, "wb")
+    for chunk in response.iter_content(chunk_size=1024):
+        if chunk:
+            handle.write(chunk)
+    return target_path
+
+def download_document(index, id, routing, cookies):
+    # Build the document URL
+    url = build_document_src_url(index, id, routing)
+    target_path = build_document_target_path(index, id, routing)
+    # Ensure the file style exist
+    if not Path(target_path).exists():
+        download_document_with_steam(url, target_path, cookies)
+    return target_path
+
+def check_user_authorization(index, id, routing, cookies):
+    url = build_document_meta_url(index, id, routing)
+    response = requests.get(url, cookies=cookies)
+    # Raise exception if the document request didn't succeed
+    if response.status_code == 401: raise DocumentUnauthorized()
+    # Find the content type and content length in nested attributes
+    content_type = response.json().get('_source', {}).get('contentType', None)
+    content_length = response.json().get('_source', {}).get('contentLength', 0)
+    # Raise exception if the contentType is not previewable
+    if not is_content_type_previewable(content_type): raise DocumentNotPreviewable()
+    # Raise exception if the contentType is not previewable
+    if content_length > DS_DOCUMENT_MAX_SIZE: raise DocumentTooBig()
