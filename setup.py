26dec8acba9d0c82b3f3032030c9ef703246e201
Bruno Thomas <bthomas@icij.org>
2019-09-13 08:44:28 +0000

    Bump version: 0.1.3 → 0.2.0

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ requires = [
 ]
 
 setup(name='datashare-preview',
-      version='0.1.3',
+      version='0.2.0',
       description="App to show document previews with a backend Elasticsearch",
       long_description=README,
       classifiers=[
