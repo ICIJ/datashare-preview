80355ec35babd6a36f6e2564b5f9864d6dc4deb4
Pierre Romera <hello@pirhoo.com>
2019-11-06 16:05:30 +0100

    Bump version: 0.4.3 → 0.5.0

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ dev_requires = [
 ]
 
 setup(name='datashare-preview',
-      version='0.4.3',
+      version='0.5.0',
       description="App to show document previews with a backend Elasticsearch",
       long_description=README,
       classifiers=[
