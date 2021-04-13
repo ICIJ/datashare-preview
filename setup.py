9d0179438d4d1f9a3ecdd8feb94ae31a3899f5c8
Pierre Romera <hello@pirhoo.com>
2021-04-13 14:29:17 +0200

    Bump version: 0.9.0 → 0.10.0

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ dev_requires = [
 ]
 
 setup(name='datashare-preview',
-      version='0.9.0',
+      version='0.10.0',
       description="App to show document previews with a backend Elasticsearch",
       long_description=README,
       classifiers=[
